a5d86c407d0bd25689966500e5a9982a284f9a9b
Danohmite <Danomite84@gmail.com>
2021-03-22 17:44:50 -0700

    Intro to Progamming

---
+++ Semantic-HTML/intro-programming/scripts/app2.js
@@ -0,0 +1,69 @@
+// const ourVar = 'some string of text';
+
+// console.log(ourVar);
+
+// let = ourVarThatIsNew = 'some new string';
+
+// console.log(ourVarThatIsNew);
+
+// const ourNum = 100;
+// const numOfStudents = 33;
+// const sizeOfGroups = 4;
+
+// const numOfGroup = numOfStudents / sizeOfGroups;
+
+// console.log(Math.floor(numOfGroup));
+
+// BOOLEAN
+
+// const newVar = 100 + ' dat';
+
+// const typeOfOurVar = typeof newVar;
+
+const areTheyEqual = 1 == '1';
+// TRUE
+
+const areTheyNotEqual = 1 !== 2;
+// TRUE
+
+const pi = Math.PI;
+const randomNum = Math.random();
+const roundedDown = Math.floor(randomNum);
+const roundedUp = Math.ceil(randomNum);
+
+// Interpolation
+
+const adjective = 'really hot';
+
+
+// WHILE LOOP
+// console.log('The number is:' + 1);
+
+// let num = 1;
+
+// while (num <= 1000) { // CONDITION
+//     console.log('The number is: ' + num); // ARGUMENT
+//      num++;
+
+// }
+
+// THIS IS AN OPENE ENDED LOOP
+// let newNum = 1;
+
+// while (newNum <= 5) {
+//     console.log('The number is ' + newNum)
+//     newNum + 1 <= 5;
+// }
+
+// let num = 1000;
+
+// while (num >= 0) { // BOOLEAN EXPRESSION 
+//     console.log(' The number ' + num);
+//     num -= 5;
+// }
+
+// FOR LOOP
+
+for (let i=1; i <=1000; i++) {
+    console.log('The number is ' + i);
+}
